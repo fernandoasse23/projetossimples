b5a94faac8ade3f1ee9f4c7c6978cbaef8c6d67c
github.com/fernandoasse23 <belamycooper16@gmail.com>
2024-08-20 18:04:51 +0100

    update atualização do code

--- form-login/login.js
+++ form-login/login.js
@@ -1,5 +1,19 @@
-/*document.querySelector('form').addEventListener('click', (e)=>{
-  //e.preventDefault()
+  document.getElementById('formulario').addEventListener('click', (e)=>{
+    
+  e.preventDefault();
+});
+
+const button = document.querySelector('.btn');
+const email = document.querySelector('#Email');
+const password = document.querySelector('#Password');
+const lab = document.querySelector('.form-control')
+
+button.addEventListener('click', ()=>{
   
-  console.log('form anulado')
-})*/
+  if (email.value == '') {
+    email.classList.add('is-invalid')
+  } else {
+    email.classList.add('is-valid')
+  }
+})
+
